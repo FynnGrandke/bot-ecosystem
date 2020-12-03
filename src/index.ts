b377c3d7109fd60d832e6e9fd5f9952cb965d9f9
Fynn Grandke <fynn.grandke@web.de>
2020-12-03 22:27:48 +0900

    Change text of openeing issue

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    const issueComment = context.issue({ body: 'Thank you Humand for opening this issue.' })
     await context.github.issues.createComment(issueComment)
   })
   // For more information on building apps:
