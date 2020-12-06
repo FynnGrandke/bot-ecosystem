bd5234535620ed9d88d559e1a382b5a586be1751
Fynn Grandke <fynn.grandke@web.de>
2020-12-06 20:13:56 +0900

    Fix deprecated function call and speeling mistake

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
-export = (app: Application) => {
+export = ({ app }: { app: Application }) => {
   app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thank you Humand for opening this issue.' })
+    const issueComment = context.issue({
+      body: 'Thank you Human for opening this issue.',
+    })
     await context.github.issues.createComment(issueComment)
   })
   // For more information on building apps:
